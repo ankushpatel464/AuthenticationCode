df5df4afc8d46d1d9c42a8e6ce47bf564e2f904b
Ankush <ankushpatel464@gmail.com>
2023-02-15 21:36:47 +0530

    MD5

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { ppid } = require("process");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-
-console.log(process.env.SECRET);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,8 +19,6 @@ const userSchema = new mongoose.Schema({
     email:String,
     password:String
 });
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -38,24 +35,27 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const newUser = new User ({
-        email:req.body.username,
-        password:req.body.password
-    });
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User ({
+            email:req.body.username,
+            password:hash
+        });
+    
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render("secrets");
+            }
+        });
     });
 });
 
 app.post("/login",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email:username},function(err,foundUser){
         if(err){
